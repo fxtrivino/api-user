59ce9e9ced896421a6397036d1631f9d97486f92
Xavier Trivino <fxtrivino@gmail.com>
2025-09-24 16:28:59 -0500

    commit 3

--- src/main/java/com/bci/service/UserService.java
+++ src/main/java/com/bci/service/UserService.java
@@ -9,9 +9,9 @@ import com.bci.dto.UsuarioDTO;
 import com.bci.entity.Usuario;
 
 public interface UserService {
-	public UsuarioDTO createUser(@RequestBody Usuario usuario);
+	public UsuarioDTO createUser(Usuario usuario);
 	public List<Usuario> getUsers();
-	public Usuario getUserById(@PathVariable String id);
-	public Usuario updateUser(@PathVariable String id, @RequestBody Usuario userDetails);
-	public void deleteUser(@PathVariable String id);
+	public Usuario getUserById(String id);
+	public Usuario updateUser(String id, Usuario userDetails);
+	public void deleteUser(String id);
 }
